8cb7e232e88dabc9763f1ba1eef1a66ba74ce44f
Dmytro Tkachenko <krikgod151019@gmail.com>
2026-02-08 20:42:03 +0200

    Пофіксив логи з railway config.py database.py, має тепер працювати

--- config.py
+++ config.py
@@ -4,4 +4,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 ADMIN_IDS = [641603995]  # твій Telegram ID
 
-DATABASE_URL = os.getenv("postgresql://postgres:qgwHHHyQXPKPlSEzYvGCicmKOeTOjfgL@postgres.railway.internal:5432/railway")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
--- database.py
+++ database.py
@@ -1,9 +1,34 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from config import DATABASE_URL
+import os
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
+from sqlalchemy.orm import DeclarativeBase
+
+# беремо DATABASE_URL з Railway
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
+
+# 👉 перетворюємо URL під asyncpg
+DATABASE_URL = DATABASE_URL.replace(
+    "postgresql://",
+    "postgresql+asyncpg://"
+)
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+)
 
 AsyncSessionLocal = async_sessionmaker(
-    engine,
-    expire_on_commit=False
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
 )
+
+class Base(DeclarativeBase):
+    pass
